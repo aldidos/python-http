7b663609328378a1d06262ed631c6aa6d40bba8a
jungil kim <jikim424@gmail.com>
2025-02-03 17:11:02 +0900

    Added new requests for testing.

---
+++ dt_server/req_exerciselib_bodypart.py
@@ -0,0 +1,12 @@
+from config import base_url, headers
+import requests
+import json
+
+def get_req() : 
+    uri = f'{base_url}/exerciselib_bodyparts'
+
+    res = requests.get(uri, headers = headers)
+    print(res.status_code)
+    print(res.text)
+
+get_req()
---
+++ dt_server/req_user_recent_workout_metrics.py
@@ -0,0 +1,13 @@
+from config import base_url, headers
+import requests
+import json
+
+def get_req() : 
+    user_id = 1
+    uri = f'{base_url}/users/{user_id}/recent_workout'
+
+    res = requests.get(uri, headers = headers)
+    print(res.status_code)
+    print(res.text)
+
+get_req()
